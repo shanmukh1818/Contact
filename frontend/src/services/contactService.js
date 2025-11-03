eccb2acc5652953cf20d8ffdfa6080a0204db3fb
shanmukh1818 <236m1a0520@bvcr.edu.in>
2025-11-03 10:30:40 +0530

    Update contactService.js

--- frontend/src/services/contactService.js
+++ frontend/src/services/contactService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api' || 'srv-d443bu3ipnbc73cg0o9g';
 
 const api = axios.create({
   baseURL: API_URL,
